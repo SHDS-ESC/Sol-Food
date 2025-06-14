18a29e883b605fca2a82aab3d6173e3c53451838
jiwon <pjw99032986@gmail.com>
2025-06-14 10:45:27 +0900

    Refactor: IP, Port, Kakao API Key 프로퍼티에서 가져오기, 트랜잭션 설정
    - 서버포트, IP, kakao RESTAPI Key 프로퍼티 파일에서 가져와 세팅
    - 카카오 로그인 트랜잭션 설정

--- src/main/java/kr/co/solfood/login/LoginController.java
+++ src/main/java/kr/co/solfood/login/LoginController.java
@@ -1,12 +1,17 @@
 package kr.co.solfood.login;
 
 import configuration.KakaoProperties;
+import configuration.ServerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -17,15 +22,25 @@ public class LoginController {
     @Autowired
     private KakaoProperties kakaoProperties;
 
-    @GetMapping("/user/login")
-    public void login(){}
+    @Autowired
+    private ServerProperties serverProperties;
 
+    @GetMapping("/user/login")
+    public void login(Model model) {
+        model.addAttribute("apiKey", kakaoProperties.getRestApiKey());
+        Map<String,String> serverMap = new HashMap<>();
+        serverMap.put("ip", serverProperties.getIp());
+        serverMap.put("port", serverProperties.getPort());
+        model.addAttribute("serverMap", serverMap);
+    }
+
+    @Transactional
     @GetMapping("/user/kakaoLogin")
     public String kakaoLogin(@RequestParam String code, HttpSession sess) {
         LoginVO kakaoLogin = service.confirmAccessToken(code);
         service.kakaoLogin(kakaoLogin);
         sess.setAttribute("loginSession", kakaoLogin);
-        return "redirect:index";
+        return "redirect:mypage";
     }
 
 }
