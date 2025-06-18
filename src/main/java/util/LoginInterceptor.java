b13cc8cabb022e9c793f3e334e994b3261d21326
jiwon <pjw99032986@gmail.com>
2025-06-18 10:40:32 +0900

    Feat : 카카오 로그인, 추가 정보 페이지 컨트롤러
    User Interceptor 추가, user 페이지 접근 권한 지정

--- src/main/java/util/LoginInterceptor.java
+++ src/main/java/util/LoginInterceptor.java
@@ -1,6 +1,6 @@
 package util;
 
-import kr.co.solfood.login.LoginVO;
+import kr.co.solfood.user.login.LoginVO;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,21 +8,22 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.PrintWriter;
 
-public class Interceptor implements HandlerInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 로그인 체크
         HttpSession session = request.getSession();
-        LoginVO vo = (LoginVO) session.getAttribute("loginSession");
+        LoginVO vo = (LoginVO) session.getAttribute("user");
         if(vo == null) {
             // 미 로그인 상태
             response.setContentType("text/html;charset=utf-8");
             PrintWriter out = response.getWriter();
             out.println("<script>");
             out.println("alert('로그인 후 이용 가능합니다.');");
-            out.println("location.href = '/user/login';");
+            out.println("location.href = '/solfood/user/login';");
             out.println("</script>");
             return false; // 가지 못함
         }
+
         // 로그인 된 상태
         return true; // 이 값이 false면 못 감 (흰 화면)
     }
