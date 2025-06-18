adda1df785e934e0c3127b01023a1652574ce24a
jiwon <pjw99032986@gmail.com>
2025-06-18 16:32:54 +0900

    Feat : Controller
    Admin, Owner Controller 세팅

---
+++ src/main/java/kr/co/solfood/admin/login/AdminLoginController.java
@@ -0,0 +1,37 @@
+package kr.co.solfood.admin.login;
+
+import configuration.KakaoProperties;
+import configuration.ServerProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminLoginController {
+
+    @Autowired
+    private KakaoProperties kakaoProperties;
+
+    @Autowired
+    private ServerProperties serverProperties;
+
+    @GetMapping("/home")
+    public void home(Model model) {
+    }
+
+    // 유저 로그인 페이지
+    @GetMapping("/login")
+    public void login(Model model) {
+        model.addAttribute("apiKey", kakaoProperties.getRestApiKey());
+        Map<String, String> serverMap = new HashMap<>();
+        serverMap.put("ip", serverProperties.getIp());
+        serverMap.put("port", serverProperties.getPort());
+        model.addAttribute("serverMap", serverMap);
+    }
+}
