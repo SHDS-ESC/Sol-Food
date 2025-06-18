57aff161542df9e0f571914ba3f92b2f3300f045
dlsundn <sw020406@naver.com>
2025-06-18 17:10:30 +0900

    Feat : StoreServiceImpl.java
    StoreServiceImpl 생성

---
+++ src/main/java/kr/co/solfood/store/StoreServiceImpl.java
@@ -0,0 +1,24 @@
+package kr.co.solfood.store;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StoreServiceImpl implements StoreService {
+
+    @Autowired
+    private StoreMapper mapper;
+
+    @Override
+    public List<StoreVO> getAllStore() {
+        return mapper.selectAllStore();
+    }
+
+    @Override
+    public List<StoreVO> getCategoryStore(String category) {
+        return mapper.selectCategoryStore(category);
+    }
+
+}
