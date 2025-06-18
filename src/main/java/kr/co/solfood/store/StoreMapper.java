645d181b916700b2cbc425b317809e5ebf8b04b4
dlsundn <sw020406@naver.com>
2025-06-18 17:06:30 +0900

    Feat : StoreMapper.java
    가게 mapper 생성

---
+++ src/main/java/kr/co/solfood/store/StoreMapper.java
@@ -0,0 +1,14 @@
+package kr.co.solfood.store;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface StoreMapper {
+    //전체 가게 목록 조회
+    List<StoreVO> selectAllStore();
+
+    //카테고리별 가게 목록 조회
+    List<StoreVO> selectCategoryStore(String category);
+}
