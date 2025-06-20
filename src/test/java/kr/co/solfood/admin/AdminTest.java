9e2514b5c475f23d929dc551fa50bec1916a73c0
jiwon <pjw99032986@gmail.com>
2025-06-20 16:29:49 +0900

    Feat : Admin Test 코드 작성
    Admin Test 코드 작성

---
+++ src/test/java/kr/co/solfood/admin/AdminTest.java
@@ -0,0 +1,54 @@
+package kr.co.solfood.admin;
+
+import kr.co.solfood.admin.home.AdminMapper;
+import kr.co.solfood.user.login.LoginMapper;
+import kr.co.solfood.user.login.UserVO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {configuration.MvcConfig.class})
+@WebAppConfiguration
+public class AdminTest {
+
+    @Autowired
+    AdminMapper adminMapper;
+
+    @Autowired
+    LoginMapper loginMapper;
+
+    @Test
+    @DisplayName("유저 홈 리스트 조회 (전체)테스트")
+    public void getUsersListAll(){
+        List<UserVO> list = adminMapper.getUsers("");
+        assumeTrue(!list.isEmpty());
+    }
+
+    @Test
+    @DisplayName("유저 홈 리스트 검색 (특정 쿼리)테스트 - 리스트 수 반환")
+    public void getUsersSize(){
+        List<UserVO> list = adminMapper.getUsers("박지원");
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    @DisplayName("유저 홈 리스트 검색 (특정 쿼리)테스트 - 리스트 검색 결과 검증")
+    public void getUsersName(){
+        String name = "안민석";
+        List<UserVO> list = adminMapper.getUsers(name);
+        assertEquals(name, list.get(0).getUsersName());
+    }
+
+}
