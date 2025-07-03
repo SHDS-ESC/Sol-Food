65596a0b3085e24859defeab68115974ce9ed311
jiwon <pjw99032986@gmail.com>
2025-07-03 10:23:36 +0900

    Feat: 정지된 사용자 Interceptor
    Interceptor에서 정지된 사용자 판별

--- src/main/java/kr/co/solfood/user/login/UserVO.java
+++ src/main/java/kr/co/solfood/user/login/UserVO.java
@@ -25,6 +25,7 @@ public class UserVO {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private Date usersUpdatedAt;
     private String usersStatus;
+    private String usersRejectedReason;
     private String usersBirth;
     private String usersLoginType;
     private String companyName; // 부서 이름
