e0ff209b1486ede34eb4f7d7e7c88dbb695440ff
SYL <yunisy51@gmail.com>
2025-07-04 17:45:38 +0900

    주석 제거

--- src/main/java/kr/co/solfood/user/mypage/MypageController.java
+++ src/main/java/kr/co/solfood/user/mypage/MypageController.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,7 +23,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 
-//@RequestMapping("/user/mypage")
 @Controller
 @RequestMapping(UrlConstants.User.MYPAGE_BASE)
 public class MypageController {
