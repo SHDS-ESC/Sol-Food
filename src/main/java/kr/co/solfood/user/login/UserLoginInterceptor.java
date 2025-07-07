0e4fc04036d351925363e1f7e6d2c5cdc0efa8a8
SYL <yunisy51@gmail.com>
2025-07-07 10:40:33 +0900

    userStatus.equals 에러 해결

--- src/main/java/kr/co/solfood/user/login/UserLoginInterceptor.java
+++ src/main/java/kr/co/solfood/user/login/UserLoginInterceptor.java
@@ -13,14 +13,27 @@ public class UserLoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 로그인 체크
         HttpSession session = request.getSession();
-        if (session.getAttribute(UrlConstants.Session.USER_LOGIN_SESSION) == null) {
-            response.setContentType("text/html; charset=UTF-8");
-            PrintWriter out = response.getWriter();
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        UserVO user = (UserVO) session.getAttribute(UrlConstants.Session.USER_LOGIN_SESSION);
+        String contextPath = request.getContextPath();
+        if (user == null) {
+            out.println("<script>");
             out.println("alert('로그인이 필요합니다.');");
 
             // Context Path를 동적으로 가져오기
             String contextPath = request.getContextPath();
             out.println("location.href = '" + contextPath + "/user/login';");
+            out.println("</script>");
+            out.flush();
+            return false;
+        } else if (UserStatus.INACTIVE.getStatus().equals(user.getUsersStatus())) {
+            out.println("<script>");
+            out.print("alert('");
+            out.print(user.getUsersRejectedReason());
+            out.print("');");
+            out.println("location.href = '" + contextPath + "/user/login';");
+            out.println("</script>");
             out.flush();
             return false;
         }
