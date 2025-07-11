e7367bb9e4e2292edfd9b1dd1852e86418f0862e
SYL <yunisy51@gmail.com>
2025-07-11 13:59:44 +0900

    비장의 무기 그거 -> 결제 내역 페이지로

--- src/main/java/kr/co/solfood/user/mypage/MypageController.java
+++ src/main/java/kr/co/solfood/user/mypage/MypageController.java
@@ -57,13 +57,6 @@ public class MypageController {
         if(userVO == null){ // 로그인 안되어있으면
             return "/user/mypage/non-user-mypage"; // 여기로 바로 포워딩 (리다이렉트x)
         }
-        if(loginService.getUserStatusById(userVO.getUsersId()).equals("inactive")){
-            // 비활성화된 계정인 경우
-            model.addAttribute("msg", "비활성화된 계정입니다. 관리자에게 문의해주세요.");
-            System.out.println(userVO);
-            System.out.println("비활성화된 계정으로 마이페이지 접근 시도: " + UrlConstants.User.LOGIN_PAGE);
-            return UrlConstants.User.LOGIN_PAGE;
-        }
         model.addAttribute("currentUser", userVO);
         return UrlConstants.View.USER_MYPAGE;
     }
@@ -237,7 +230,17 @@ public class MypageController {
     }
 
     @GetMapping("/payment-history")
-    public String paymentHistoryPage() {
+    public String paymentHistoryPage(Model model, HttpSession sess) {
+        UserVO userVO = (UserVO) sess.getAttribute(UrlConstants.Session.USER_LOGIN_SESSION);
+
+        if(loginService.getUserStatusById(userVO.getUsersId()).equals("inactive")){
+            // 비활성화된 계정인 경우
+            model.addAttribute("msg", "비활성화된 계정입니다. 관리자에게 문의해주세요.");
+            System.out.println(userVO);
+            System.out.println("비활성화된 계정으로 마이페이지 접근 시도: " + UrlConstants.User.LOGIN_PAGE);
+            return UrlConstants.User.LOGIN_PAGE;
+        }
+
         // 페이지 렌더링만 담당, 데이터는 JavaScript로 API 호출
         return "user/mypage/payment-history";
     }
